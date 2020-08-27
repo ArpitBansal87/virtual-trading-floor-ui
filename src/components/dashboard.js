fce757f6878397d6ee5949237aaeb08f15239278
Arpit Bansal <arpit.bansal@outlook.com>
2020-08-27 19:28:50 +0530

    stock Listing changes

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,24 +3,52 @@ import UserPortfolioListing from "./usersPortfolioListing";
 import StockMarket from "./stockMarket";
 import PortfolioListing from "./portfolioListing";
 import NavBarComponenet from "./navBarComponenet";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Box } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const Dashboard = () => {
+  const classes = useStyles();
+
   return (
     <div>
       <NavBarComponenet></NavBarComponenet>
-      <div className="container-content">
-        <div className="flexBox">
-          <div className="stocks-listing">
-            <StockMarket></StockMarket>
+      <Box pt={4}>
+        <Container>
+          <div className="container-content">
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={8}>
+                <Paper className={classes.paper}>
+                  <StockMarket></StockMarket>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Paper className={classes.paper}>
+                  <PortfolioListing></PortfolioListing>
+                </Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                  <UserPortfolioListing></UserPortfolioListing>
+                </Paper>
+              </Grid>
+            </Grid>
           </div>
-          <div className="portfolio-listing">
-            <PortfolioListing></PortfolioListing>
-          </div>
-        </div>
-        <div className="user-listing">
-          <UserPortfolioListing></UserPortfolioListing>
-        </div>
-      </div>
+          <div className="user-listing"></div>
+        </Container>
+      </Box>
     </div>
   );
 };
--- src/components/stock.js
+++ src/components/stock.js
@@ -1,4 +1,23 @@
 import React, { useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  logoName: {
+    height: 100,
+    width: 100,
+  }
+});
+
 
 const Stock = (props) => {
   // TODO: give the user the option to cancel the trade
@@ -53,11 +72,36 @@ const Stock = (props) => {
       setTradeType(event.target.value);
   } 
 
+  const classes = useStyles();
+
   return (
     <>
-      <div>
-        <span>{props.data.companyName}</span>
-        <span>{props.data.sharePrice}</span>
+    
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={props.data.companyName}
+          className={classes.logoName}
+          image={props.data.logoImage}
+          title={props.data.symbol}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+          {props.data.companyName}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            $ {props.data.sharePrice}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        {/* <Button size="small" color="primary">
+          Trade
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button> */}
         <button onClick={handleTradeToggle}>{openForTrade ? 'Confirm' :'Buy/Sell'}</button>
 
         {openForTrade ? (
@@ -88,8 +132,9 @@ const Stock = (props) => {
         ) : (
           <></>
         )}
-      </div>
-    </>
+      </CardActions>
+    </Card>
+     </>
   );
 };
 
