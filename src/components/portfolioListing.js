c7722937e3090b045ee4bb3f762a603c0a5ebe6e
Arpit Bansal <arpit.bansal@outlook.com>
2020-08-30 06:54:22 +0530

    UI Changes for holdings and stocks

--- src/components/portfolioListing.js
+++ src/components/portfolioListing.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
 import PortFolioElement from "./portfolioElement";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { Typography } from "@material-ui/core";
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
+import TrendingDownIcon from "@material-ui/icons/TrendingDown";
 
 const PortfolioListing = () => {
   const [portfolioStocks, setportfolioStocks] = useState({ response: [] });
+  const [holdingsValue, setHoldingsValue] = useState({
+    current: 0,
+    invested: 0,
+  });
 
   useEffect(() => {
     const client = new W3CWebSocket(
@@ -18,6 +25,16 @@ const PortfolioListing = () => {
     client.onmessage = (message) => {
       const responseObj = JSON.parse(message.data);
       setportfolioStocks(responseObj);
+      let currentValue = 0;
+      let investedValue = 0;
+      responseObj.response.forEach((holding) => {
+        currentValue += holding.currentPrice * holding.tradeQuantity;
+        investedValue += holding.avgPrice * holding.tradeQuantity;
+      });
+      setHoldingsValue({
+        current: currentValue,
+        invested: investedValue,
+      });
     };
 
     client.onclose = () => {
@@ -25,21 +42,35 @@ const PortfolioListing = () => {
     };
   }, []);
   return (
-    <div>
-      <ul>
+    <>
+      <div>
+        <h3>Holdings({portfolioStocks.response.length}): </h3>
+        <div style={{ textAlign: "left", display: 'flex', justifyContent: 'space-around' }}>
+          <div>
+            <Typography>
+              Current: {Number.parseFloat(holdingsValue.current).toFixed(2)}
+            </Typography>
+            <Typography>
+              Invested: {Number.parseFloat(holdingsValue.invested).toFixed(2)}
+            </Typography>
+          </div>
+          <div>
+            {holdingsValue.current > holdingsValue.invested ? (
+              <TrendingUpIcon fontSize="large" style={{ color: "green",  }} />
+            ) : (
+              <TrendingDownIcon fontSize="large" style={{ color: "red" }} />
+            )}
+          </div>
+        </div>
         {portfolioStocks.response.length === 0 ? (
-          <li>
-            <h3>No Portfolios Loaded</h3>
-          </li>
+          <h3>No Portfolios Loaded</h3>
         ) : (
           portfolioStocks.response.map((portfolioStock, index) => (
-            <li key={index}>
-              <PortFolioElement data={portfolioStock}></PortFolioElement>
-            </li>
+            <PortFolioElement data={portfolioStock}></PortFolioElement>
           ))
         )}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 };
 
--- src/components/stockMarket.js
+++ src/components/stockMarket.js
@@ -4,6 +4,9 @@ import Stock from "./stock";
 import { makeStyles } from "@material-ui/core/styles";
 import MaterialTable from "material-table";
 import { Button } from "@material-ui/core";
+import ViewModuleIcon from "@material-ui/icons/ViewModule";
+import ViewStreamIcon from "@material-ui/icons/ViewStream";
+import { blue } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
   root: {
@@ -17,11 +20,16 @@ const useStyles = makeStyles({
     display: "flex",
     flexWrap: "wrap",
     padding: 0,
+    margin: 0
   },
   liValue: {
     maxHeight: 400,
     minWidth: 245,
     margin: "1rem",
+  },
+  isActive: {
+    fontSize: 48,
+    color: "blue",
   },
 });
 
@@ -29,6 +37,8 @@ const StockMarket = () => {
   const classes = useStyles();
 
   const [stocksList, setStocksList] = useState({ response: [] });
+  const [gridView, setGridView] = useState(true);
+
   useEffect(() => {
     const client = new W3CWebSocket(
       process.env.REACT_APP_WS_API_URL + "/stocks"
@@ -44,55 +54,72 @@ const StockMarket = () => {
   }, []);
   return (
     <>
-      <h3>Stocks</h3>
       <div>
-        <ul className={classes.ulValue}>
-          {stocksList.response.length === 0 ? (
-            <li>
-              <h3>No Stocks Loaded</h3>
-            </li>
-          ) : (
-            stocksList.response.map((stock, index) => (
-              <li key={index} className={classes.liValue}>
-                <Stock data={stock} />
-              </li>
-            ))
-          )}
-        </ul>
+        <div style={{ float: "right" }}>
+          <ViewModuleIcon
+            fontSize="large"
+            style={{ cursor: "pointer" }}
+            className={gridView ? classes.isActive : classes.isInactive}
+            onClick={() => setGridView(true)}
+          />
+          <ViewStreamIcon
+            fontSize="large"
+            style={{ cursor: "pointer" }}
+            className={gridView ? classes.isInactive : classes.isActive}
+            onClick={() => setGridView(false)}
+          />
+        </div>
+        <h3>Stocks</h3>
       </div>
 
-      <div style={{ maxWidth: "100%" }}>
-        <MaterialTable
-          columns={[
-            {
-              title: "Logo",
-              field: "logoImage",
-              render: (data) => (
-                <img
-                  alt="data.companyName"
-                  src={data.logoImage}
-                  style={{ width: 50, height: 50 }}
-                ></img>
-              ),
-            },
-            { title: "Company Name", field: "companyName" },
-            { title: "Share Price", field: "sharePrice" },
-            {
-              title: "Trade Options",
-              field: "tradeOption",
-              render: (data) => (
-                <>
-                  <Button size="small" color="primary">
-                    Trade
-                  </Button>
-                </>
-              ),
-            },
-          ]}
-          data={stocksList.response}
-          title="Stocks"
-        />
-      </div>
+      {gridView ? (
+          <div className={classes.ulValue}>
+            {stocksList.response.length === 0 ? (
+              <div>
+                <h3>No Stocks Loaded</h3>
+              </div>
+            ) : (
+              stocksList.response.map((stock, index) => (
+                <div key={index} className={classes.liValue}>
+                  <Stock data={stock} />
+                </div>
+              ))
+            )}
+          </div>
+      ) : (
+        <div style={{ maxWidth: "100%" }}>
+          <MaterialTable
+            columns={[
+              {
+                title: "Logo",
+                field: "logoImage",
+                render: (data) => (
+                  <img
+                    alt="data.companyName"
+                    src={data.logoImage}
+                    style={{ width: 50, height: 50 }}
+                  ></img>
+                ),
+              },
+              { title: "Company Name", field: "companyName" },
+              { title: "Share Price", field: "sharePrice" },
+              {
+                title: "Trade Options",
+                field: "tradeOption",
+                render: (data) => (
+                  <>
+                    <Button size="small" color="primary">
+                      Trade
+                    </Button>
+                  </>
+                ),
+              },
+            ]}
+            data={stocksList.response}
+            title="Stocks"
+          />
+        </div>
+      )}
     </>
   );
 };
